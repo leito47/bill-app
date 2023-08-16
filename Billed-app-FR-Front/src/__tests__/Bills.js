96cad1f006865232f7110a495d5846e048a3f86a
Gautier Laurent <gautierlaurent47@gmail.com>
2023-08-16 18:58:15 +0200

    test unit bills and newbills

--- Billed-app-FR-Front/src/__tests__/Bills.js
+++ Billed-app-FR-Front/src/__tests__/Bills.js
@@ -3,12 +3,18 @@
  */
 
 import {screen, waitFor} from "@testing-library/dom"
+import userEvent from "@testing-library/user-event"
 import BillsUI from "../views/BillsUI.js"
 import { bills } from "../fixtures/bills.js"
-import { ROUTES_PATH} from "../constants/routes.js";
-import {localStorageMock} from "../__mocks__/localStorage.js";
-
+import Bills from "../containers/Bills.js";
+import { ROUTES, ROUTES_PATH} from "../constants/routes.js";
+import {localStorageMock } from "../__mocks__/localStorage.js";
+import mockStore from "../__mocks__/store"
 import router from "../app/Router.js";
+
+import { NewBill } from '../containers/NewBill.js'
+
+jest.mock("../app/Store", () => mockStore)
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
@@ -26,8 +32,27 @@ describe("Given I am connected as an employee", () => {
       await waitFor(() => screen.getByTestId('icon-window'))
       const windowIcon = screen.getByTestId('icon-window')
       //to-do write expect expression
+         expect(windowIcon.classList.contains("active-icon")).toBe(true) 
 
     })
+    test("Then bill icon in vertical layout should be highlighted", async () => {
+
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee'
+      }))
+      const root = document.createElement("div")
+      root.setAttribute("id", "root")
+      document.body.append(root)
+      router()
+      window.onNavigate(ROUTES_PATH.Bills)
+      await waitFor(() => screen.getByTestId('icon-mail'))
+      const windowIcon = screen.getByTestId('icon-mail')
+      //to-do write expect expression
+         expect(windowIcon.classList.contains("active-icon")).toBe(false) 
+
+    })
+
     test("Then bills should be ordered from earliest to latest", () => {
       document.body.innerHTML = BillsUI({ data: bills })
       const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
@@ -36,4 +61,91 @@ describe("Given I am connected as an employee", () => {
       expect(dates).toEqual(datesSorted)
     })
   })
+
+  // Testing the icon eye
+  describe('When I click on the eye icon of one bill', () => {
+    test('Then a modal should open', async () => {
+      const onNavigate = pathname => { document.body.innerHTML = ROUTES({ pathname }); };
+      Object.defineProperty(window, "localStorage", { value: localStorageMock });
+      window.localStorage.setItem("user", JSON.stringify({ type: "Employee" }));
+      document.body.innerHTML = BillsUI({ data: bills })
+      const containerBills = new Bills({
+        document,
+        onNavigate,
+        store: null,
+        localStorage: window.localStorage
+      })
+
+      const modaleFile = document.getElementById('modaleFile')
+      $.fn.modal = jest.fn(() => modaleFile.classList.add('show'))
+
+      const eyeIcon = await screen.getAllByTestId('icon-eye')
+
+      const handleClickIconEye = jest.fn((icon)=> containerBills.handleClickIconEye(icon))
+ 
+      eyeIcon.forEach(icon => {
+      icon.addEventListener('click', handleClickIconEye(icon))
+      userEvent.click(icon)
+      expect(handleClickIconEye).toHaveBeenCalled()
+      })
+      expect(modaleFile).toBeTruthy()
+    })
+  })
+  // TEST D'INTEGRATION GET
+describe('Given I am connected as employee', () => {
+  describe('When I am on the Bills page', () => {
+    test('Then we fetch the bills from mock API GET', async () => {
+      localStorage.setItem('user', JSON.stringify({ type: 'Employee, e@e'}))
+      const root = document.createElement('div')
+      root.setAttribute('id', 'root')
+      document.body.appendChild(root)
+
+      router()
+      window.onNavigate(ROUTES_PATH.Bills)
+      await waitFor(() => screen.getAllByText('Mes notes de frais'))
+
+      expect(screen.getByTestId('tbody')).toBeTruthy()
+    })
+  })
+
+  describe('When an error occurs in API', () => {
+    beforeEach(() => {
+      jest.spyOn(mockStore, 'bills')
+      Object.defineProperty(window, 'localStorage', {value: localStorageMock})
+      window.localStorage.setItem('user', JSON.stringify( {type: 'Employee', email: 'e@e'}))
+      const root = document.createElement('div')
+      root.setAttribute('id', 'root')
+      document.body.appendChild(root)
+      router()
+    })
+
+    test('Then we fetch the bills from the API and this fails 404 error message', async () => {
+      mockStore.bills.mockImplementationOnce(() => {
+        return { list: () => { return Promise.reject(new Error('Erreur 404')); }}
+      })
+    // })
+
+    window.onNavigate(ROUTES_PATH.Bills)
+    await new Promise(process.nextTick)
+    const message = await screen.getByText(/Erreur 404/)
+    expect(message).toBeTruthy()
+  })
+
+  test('Then we fetch the message from API an we have in return a 500 message error', async ()=> {
+      mockStore.bills.mockImplementationOnce(() => {
+        return { list: () => { return Promise.reject(new Error('Erreur 500')); }}
+      })
+
+        window.onNavigate(ROUTES_PATH.Bills)
+        await new Promise(process.nextTick)
+        const message = await screen.getByText(/Erreur 500/)
+        expect(message).toBeTruthy()
+      })
+  })
 })
+
+
+
+})
+
+
--- Billed-app-FR-Front/src/__tests__/NewBill.js
+++ Billed-app-FR-Front/src/__tests__/NewBill.js
@@ -2,17 +2,152 @@
  * @jest-environment jsdom
  */
 
-import { screen } from "@testing-library/dom"
+import { screen, fireEvent, waitFor , render} from "@testing-library/dom" // Ajout fireEvent (déclenchement action sur DOM) & waitFor (Async)
+
+import { ROUTES, ROUTES_PATH } from "../constants/routes.js"
+import { localStorageMock } from "../__mocks__/localStorage.js"
+
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
+import '@testing-library/jest-dom'
+import mockStore from "../__mocks__/store"
+import router from "../app/Router.js" 
 
+import userEvent from "@testing-library/user-event"
+jest.mock("../app/Store", () => mockStore)
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
-      //to-do write assertion
+    test("Then icon of mail in the vertical Layout must be highlighted", async () => {
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem('user',  JSON.stringify({ type: 'Employee'}))
+      const root = document.createElement('div')
+      root.setAttribute('id', 'root')
+      document.body.append(root)
+      router();
+
+      window.onNavigate(ROUTES_PATH.NewBill)
+      await waitFor(() => screen.getByTestId('icon-mail'))
+
+      const emailIcon = screen.getByTestId('icon-mail')
+      expect(emailIcon).toBeTruthy();
+      expect(screen.getByTestId('icon-mail').classList.contains('active-icon')).toBeTruthy();
     })
+
+    test("Then the inputs forms should be displayed on the web page", () => {
+      document.body.innerHTML = NewBillUI()
+
+      const fieldTypeOfExpense= screen.getByTestId('expense-type')
+      expect(fieldTypeOfExpense).toBeInTheDocument()
+
+      const fieldExpenseName= screen.getByTestId('expense-name')
+      expect(fieldExpenseName).toBeInTheDocument()
+
+      const fieldDatePicker= screen.getByTestId('datepicker')
+      expect(fieldDatePicker).toBeInTheDocument()
+
+      const fieldAmount= screen.getByTestId('amount')
+      expect(fieldAmount).toBeInTheDocument()
+
+      const fieldVat= screen.getByTestId('vat')
+      expect(fieldVat).toBeInTheDocument()
+
+      const fieldPct= screen.getByTestId('pct')
+      expect(fieldPct).toBeInTheDocument()
+
+      const fieldCommentary= screen.getByTestId('commentary')
+      expect(fieldCommentary).toBeInTheDocument()
+
+      const fieldFile= screen.getByTestId('file')
+      expect(fieldFile).toBeInTheDocument()
+    });
+    describe("When I am on the newBill page, I fill the form and submit", () => {
+      test("Then the new bill should be added to the API POST", async() => {
+
+        const html = NewBillUI()
+        document.body.innerHTML = html
+   
+     // simulation and initialization of new bill   
+    const bill = {
+  "id": "UIUZtnPQvnbFnB0ozvJh",
+   "name": "test3",
+  "email": "a@a",
+  "type": "Services en ligne",
+  "vat": "60",
+  "pct": 20,
+  "commentAdmin": "bon bah d'accord",
+  "amount": 300,
+  "status": "accepted",
+  "date": "2003-03-03",
+  "commentary": "",
+  "fileName": "facture-client-php-exportee-dans-document-pdf-enregistre-sur-disque-dur.png",
+  "fileUrl": "https://test.storage.tld/v0/b/billable-677b6.a…dur.png?alt=media&token=571d34cb-9c8f-430a-af52-66221cae1da3"
+    }
+    const fieldTypeOfExpense= screen.getByTestId('expense-type')
+   fireEvent.change(fieldTypeOfExpense, {target: {value: bill.type}});
+    expect(fieldTypeOfExpense.value).toBe(bill.type);
+    console.log("fieldTypeOfExpense",fieldTypeOfExpense.value,bill.type)
+
+    // const fieldTypeOfExpense= screen.getByTestId('expense-type')
+    // userEvent.type(fieldTypeOfExpense,"Services en ligne", bill.type);
+    //  expect(fieldTypeOfExpense.value).toBe(bill.type);
+    //  console.log("fieldTypeOfExpense",fieldTypeOfExpense.value,bill.type)
+ 
+
+    const expenseNameField = screen.getByTestId('expense-name')
+    userEvent.type(expenseNameField, bill.name);
+    expect(expenseNameField.value).toBe(bill.name);
+    console.log("expenseNameField value:", expenseNameField.value,bill.name);
+
+    const datePickerField = screen.getByTestId('datepicker')
+   fireEvent.change(datePickerField, {target: {value: bill.date} })
+    expect(datePickerField.value).toBe(bill.date)
+    console.log("datePickerField value:", datePickerField.value,bill.date);
+
+    const amountField = screen.getByTestId('amount')
+    userEvent.type(amountField, bill.amount.toString());
+    expect(parseInt(amountField.value)).toBe(parseInt(bill.amount));
+    console.log("amountField value:",amountField.value,bill.amount);
+
+    const vatField = screen.getByTestId('vat')
+    userEvent.type(vatField, bill.vat.toString());
+    expect(parseInt(vatField.value)).toBe(parseInt(bill.vat));
+    console.log("vatField value:",vatField.value,bill.vat);
+
+    const pctField = screen.getByTestId('pct')
+    userEvent.type(pctField, bill.pct.toString());
+    expect(parseInt(pctField.value)).toBe(parseInt(bill.pct));
+    console.log("pctField value:",pctField.value,bill.pct);
+
+    const CommentaryField = screen.getByTestId('commentary')
+    userEvent.type(CommentaryField, bill.commentary);
+    expect(CommentaryField.value).toBe(bill.commentary);
+    console.log("CommentaryField value:",CommentaryField.value,bill.commentary);
+    
+
+    const onNavigate = pathname => { document.body.innerHTML =  ROUTES({ pathname})}
+    const newBillForm = screen.getByTestId('form-new-bill')
+    Object.defineProperty(window, 'localStorage', { value: localStorageMock})
+    
+    const newBill = new NewBill({document, onNavigate, store: mockStore, localStorage: window.localStorage})
+
+    const handleChangeFile = jest.fn(newBill.handleChangeFile)
+    newBillForm.addEventListener('change', handleChangeFile)
+
+    const fileField = screen.getByTestId('file')
+    fireEvent.change(fileField, {target: {files: [new File([bill.fileName], bill.fileUrl, {type: 'image/png'}) ] }})
+    expect(fileField.files[0].name).toBe(bill.fileUrl)
+    expect(fileField.files[0].type).toBe('image/png')
+    expect(handleChangeFile).toHaveBeenCalled()
+
+    const handleSubmit = jest.fn(newBill.handleSubmit);
+    newBillForm.addEventListener("submit", handleSubmit);
+    fireEvent.submit(newBillForm);
+    expect(handleSubmit).toHaveBeenCalled();
+
   })
 })
+  })
+});
+
+
